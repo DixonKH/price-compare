c67c02b74d966b7287123a5933248a474111183c
DixonKH <khaydarovdilshod9@gmail.com>
2025-12-10 00:25:50 +0900

    feat: develop AuthService and AuthController modules by adding login and regsiter apis

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_ACCESS_SECRET,
+      signOptions: { expiresIn: '15m' },
+    }),
+    PrismaModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, PrismaService],
 })
 export class AuthModule {}
--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+
+async register(dto: RegisterDto) {
+  const exist = await this.prisma.user.findUnique({
+    where: { email: dto.email },
+  });
+
+  if (exist) {
+    throw new BadRequestException('User already exists');
+  }
+
+  const hashedPassword = await bcrypt.hash(dto.password, 10);
+
+  const user = await this.prisma.user.create({
+    data: {
+      email: dto.email,
+      name: dto.name,
+      password: hashedPassword,
+    },
+  });
+  console.log("user:", user);
+  return this.issueTokens(user.id, user.email);
+}
+
+async login(dto: LoginDto) {
+  const user = await this.prisma.user.findUnique({
+    where: { email: dto.email },
+  });
+
+  if (!user) throw new UnauthorizedException('Invalid credentials');
+
+  const isValid = await bcrypt.compare(dto.password, user.password);
+  if (!isValid) throw new UnauthorizedException('Invalid credentials');
+
+  return this.issueTokens(user.id, user.email);
+}
+
+async issueTokens(userId: number, email: string) {
+  const payload = { sub: userId, email };
+
+  const accessToken = await this.jwtService.signAsync(payload);
+
+  const refreshToken = await this.jwtService.signAsync(payload, {
+    expiresIn: '7d',
+    secret: process.env.JWT_REFRESH_SECRET,
+  });
+
+  return { accessToken, refreshToken };
+}
+}
